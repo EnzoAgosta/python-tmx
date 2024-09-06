c26caa890212a9b361a9199c64091886214e5b9e
EnzoAgosta <enzo.agosta@ninjarmm.com>
2024-09-06 15:53:20 +0200

    basic implementation using pydantic models for validation

---
+++ src/converters.py
@@ -0,0 +1,208 @@
+import xml.etree.ElementTree
+
+import lxml.etree
+
+from models import (
+    Bpt,
+    Ept,
+    Header,
+    Hi,
+    It,
+    Map,
+    Note,
+    Ph,
+    Prop,
+    Sub,
+    Tmx,
+    TmxElement,
+    Tu,
+    Tuv,
+    Ude,
+    Ut,
+)
+
+XML = "{http://www.w3.org/XML/1998/namespace}"
+
+
+def from_element(
+    element: lxml.etree._Element | xml.etree.ElementTree.Element,
+) -> TmxElement:
+    def _content_from_elem(
+        element: lxml.etree._Element | xml.etree.ElementTree.Element,
+    ) -> list[str | TmxElement] | str:
+        content = [element.text]
+        for child in element:
+            content.append(from_element(child))
+            content.append(child.tail)
+        content = (
+            list(filter(lambda x: x is not None, content))
+            if len(content) > 1
+            else content[0]
+        )
+        return content
+
+    match element.tag:
+        case "note":
+            return Note(
+                content=element.text,
+                encoding=element.get("o-encoding"),
+                lang=element.get(f"{XML}lang"),
+            )
+        case "prop":
+            return Prop(
+                content=element.text,
+                type=element.get("type"),
+                lang=element.get(f"{XML}lang"),
+                encoding=element.get("o-encoding"),
+            )
+        case "map":
+            return Map(
+                unicode=element.get("unicode"),
+                code=element.get("code"),
+                ent=element.get("ent"),
+                subst=element.get("subst"),
+            )
+        case "ude":
+            maps = []
+            for child in element:
+                if not child.tag == "map":
+                    raise ValueError(
+                        f"Ude elements are only allowed to have Map elements as children but found: {child.tag}"
+                    )
+                maps.append(from_element(child))
+            return Ude(maps=maps, name=element.get("name"), base=element.get("base"))
+        case "header":
+            notes, props, udes = [], [], []
+            for child in element:
+                if child.tag == "note":
+                    notes.append(from_element(child))
+                elif child.tag == "prop":
+                    props.append(from_element(child))
+                elif child.tag == "ude":
+                    udes.append(from_element(child))
+            return Header(
+                notes=notes,
+                props=props,
+                udes=udes,
+                creationtool=element.get("creationtool"),
+                creationtoolversion=element.get("creationtoolversion"),
+                segtype=element.get("segtype"),
+                tmf=element.get("o-tmf"),
+                adminlang=element.get("adminlang"),
+                srclang=element.get("srclang"),
+                datatype=element.get("datatype"),
+                encoding=element.get("o-encoding"),
+                creationdate=element.get("creationdate"),
+                creationid=element.get("creationid"),
+                changedate=element.get("changedate"),
+                changeid=element.get("changeid"),
+            )
+        case "tu":
+            notes, props, tuvs = [], [], []
+            for child in element:
+                if child.tag == "note":
+                    notes.append(from_element(child))
+                elif child.tag == "prop":
+                    props.append(from_element(child))
+                elif child.tag == "tuv":
+                    tuvs.append(from_element(child))
+            return Tu(
+                tuvs=tuvs,
+                notes=notes,
+                props=props,
+                tuid=element.get("tuid"),
+                encoding=element.get("o-encoding"),
+                datatype=element.get("datatype"),
+                usagecount=element.get("usagecount"),
+                lastusagedate=element.get("lastusagedate"),
+                creationtool=element.get("creationtool"),
+                creationtoolversion=element.get("creationtoolversion"),
+                creationdate=element.get("creationdate"),
+                creationid=element.get("creationid"),
+                changedate=element.get("changedate"),
+                segtype=element.get("segtype"),
+                changeid=element.get("changeid"),
+                tmf=element.get("o-tmf"),
+                srclang=element.get("srclang"),
+            )
+        case "tuv":
+            notes, props, segment = [], [], []
+            for child in element:
+                if child.tag == "note":
+                    notes.append(from_element(child))
+                elif child.tag == "prop":
+                    props.append(from_element(child))
+            seg = element.find("seg")
+            segment.append(seg.text)
+            for child in seg:
+                segment.append(from_element(child))
+                segment.append(child.tail)
+            segment = (
+                list(filter(lambda x: x is not None, segment))
+                if len(segment) > 1
+                else segment[0]
+            )
+            return Tuv(
+                segment=segment,
+                notes=notes,
+                props=props,
+                lang=element.get(f"{XML}lang"),
+                encoding=element.get("o-encoding"),
+                datatype=element.get("datatype"),
+                usagecount=element.get("usagecount"),
+                lastusagedate=element.get("lastusagedate"),
+                creationtool=element.get("creationtool"),
+                creationtoolversion=element.get("creationtoolversion"),
+                creationdate=element.get("creationdate"),
+                creationid=element.get("creationid"),
+                changedate=element.get("changedate"),
+                changeid=element.get("changeid"),
+                tmf=element.get("o-tmf"),
+            )
+        case "tmx":
+            header = from_element(element.find("header"))
+            tus = [from_element(child) for child in element.iter("tu")]
+            return Tmx(header=header, tus=tus)
+        case "bpt":
+            return Bpt(
+                content=_content_from_elem(element),
+                i=element.get("i"),
+                x=element.get("x"),
+                type=element.get("type"),
+            )
+        case "ept":
+            return Ept(
+                content=_content_from_elem(element),
+                i=element.get("i"),
+            )
+        case "ph":
+            return Ph(
+                content=_content_from_elem(element),
+                assoc=element.get("assoc"),
+                x=element.get("x"),
+                type=element.get("type"),
+            )
+        case "it":
+            return It(
+                content=_content_from_elem(element),
+                pos=element.get("pos"),
+                x=element.get("x"),
+                type=element.get("type"),
+            )
+        case "hi":
+            return Hi(
+                content=_content_from_elem(element),
+                x=element.get("x"),
+                type=element.get("type"),
+            )
+        case "sub":
+            return Sub(
+                content=_content_from_elem(element),
+                datatype=element.get("datatype"),
+                type=element.get("type"),
+            )
+        case "ut":
+            return Ut(
+                content=_content_from_elem(element),
+                x=element.get("x"),
+            )
---
+++ src/models.py
@@ -0,0 +1,184 @@
+from __future__ import annotations
+
+from datetime import datetime
+from typing import Annotated, Literal, Optional, Self
+
+from pydantic import BaseModel, Field, ValidationInfo, field_validator
+
+
+class TmxElement(BaseModel):
+    @field_validator(
+        "creationdate", "changedate", "lastusagedate", mode="after", check_fields=False
+    )
+    @classmethod
+    def check_datetime(cls, timestamp: str | datetime | None) -> datetime:
+        if not timestamp:
+            return timestamp
+        if isinstance(timestamp, datetime):
+            return timestamp
+        try:
+            return datetime.strptime(timestamp, r"%Y%m%dT%H%M%SZ")
+        except TypeError as e:
+            raise ValueError(e.__cause__)
+        except ValueError as e:
+            raise e
+
+    @field_validator("segment", mode="after", check_fields=False)
+    @classmethod
+    def check_paired(
+        cls, segment: str | list[str | TmxElement]
+    ) -> str | list[str | TmxElement]:
+        if isinstance(segment, str):
+            return segment
+        if len([item for item in segment if isinstance(item, Bpt)]) != len(
+            [item for item in segment if isinstance(item, Ept)]
+        ):
+            raise ValueError(
+                "the amount of Bpt and Ept elements in a segment must be equal"
+            )
+        return segment
+
+
+class Note(TmxElement):
+    content: str
+    lang: Optional[str] = None
+    encoding: Optional[str] = None
+
+
+class Prop(TmxElement):
+    content: str
+    type: str
+    lang: Optional[str] = None
+    encoding: Optional[str] = None
+
+
+class Map(TmxElement):
+    unicode: str
+    code: Optional[str] = None
+    ent: Optional[str] = None
+    subst: Optional[str] = None
+
+
+class Ude(TmxElement):
+    maps: list[Map] = []
+    name: str
+    base: Annotated[Optional[str], Field(validate_default=True)] = None
+
+    @field_validator("base", mode="after")
+    @classmethod
+    def check_for_base(cls, base: str | None, info: ValidationInfo) -> str:
+        if base is not None:
+            return base
+        _maps = info.data["maps"]
+        if not len(_maps):
+            return base
+        for _map in _maps:
+            if _map.code:
+                raise ValueError(
+                    "a value for base is required if one or more of the Map elements contains a code attribute"
+                )
+
+
+class Header(TmxElement):
+    notes: list[Note] = []
+    props: list[Prop] = []
+    udes: list[Ude] = []
+    creationtool: str
+    creationtoolversion: str
+    segtype: Literal["block", "paragraph", "sentence", "phrase"]
+    tmf: str
+    adminlang: str
+    srclang: str
+    datatype: str
+    encoding: Optional[str] = None
+    creationdate: Optional[str | datetime] = None
+    creationid: Optional[str] = None
+    changedate: Optional[str | datetime] = None
+    changeid: Optional[str] = None
+
+
+class Tuv(TmxElement):
+    segment: str | list[str | Bpt | Ept | It | Hi | Ph] = []
+    notes: list[Note] = []
+    props: list[Prop] = []
+    lang: str
+    encoding: Optional[str] = None
+    datatype: Optional[str] = None
+    usagecount: Optional[int] = None
+    lastusagedate: Optional[str | datetime] = None
+    creationtool: Optional[str] = None
+    creationtoolversion: Optional[str] = None
+    creationdate: Optional[str | datetime] = None
+    creationid: Optional[str] = None
+    changedate: Optional[str | datetime] = None
+    changeid: Optional[str] = None
+    tmf: Optional[str] = None
+
+
+class Tu(TmxElement):
+    tuvs: list[Tuv] = []
+    notes: list[Note] = []
+    props: list[Prop] = []
+    tuid: Optional[str] = None
+    encoding: Optional[str] = None
+    datatype: Optional[str] = None
+    usagecount: Optional[int] = None
+    lastusagedate: Optional[str | datetime] = None
+    creationtool: Optional[str] = None
+    creationtoolversion: Optional[str] = None
+    creationdate: Optional[str | datetime] = None
+    creationid: Optional[str] = None
+    changedate: Optional[str | datetime] = None
+    segtype: Optional[Literal["block", "paragraph", "sentence", "phrase"]] = None
+    changeid: Optional[str] = None
+    tmf: Optional[str] = None
+    srclang: Optional[str] = None
+
+
+class Tmx(TmxElement):
+    version: str = "1.4"
+    header: Header
+    tus: list[Tu]
+
+
+class Sub(TmxElement):
+    content: str | list[Bpt | Ept | It | Ph | Hi]
+    datatype: Optional[str] = None
+    type: Optional[str] = None
+
+
+class Ut(TmxElement):
+    content: str | list[str | Sub]
+    x: Optional[int] = None
+
+
+class Bpt(TmxElement):
+    content: str | list[str | Sub]
+    i: int
+    x: Optional[int] = None
+    type: Optional[str] = None
+
+
+class Ept(TmxElement):
+    content: str | list[str | Sub]
+    i: Optional[int]
+
+
+class It(TmxElement):
+    content: str | list[str | Sub]
+    pos: Literal["begin", "end"]
+    x: Optional[int] = None
+    type: Optional[str] = None
+
+
+class Ph(TmxElement):
+    content: str | list[str | Sub]
+    assoc: Optional[Literal["p", "f", "b"]] = None
+    x: Optional[int] = None
+    type: Optional[str] = None
+
+
+class Hi(TmxElement):
+    content: str | list[str | Bpt | Ept | It | Ph | Self]
+    x: Optional[int] = None
+    type: Optional[str] = None
