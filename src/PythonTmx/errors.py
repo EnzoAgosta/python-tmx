8c9755dba5e282f86840549c6b2f572512041669
Enzo Agosta <agosta.enzowork@gmail.com>
2025-07-24 15:36:34 +0200

    improve errors

--- src/PythonTmx/errors.py
+++ src/PythonTmx/errors.py
@@ -1,19 +1,42 @@
+from typing import Any
+
+
 class ParsingError(Exception):
   tag: str
   line: str
-  element: str
   original_exception: Exception
+
+  def __str__(self):
+        return f"{super().__str__()}\nTag: {self.tag}\nLine: {self.line}\nCaused by: {self.original_exception}"
 
   def __init__(
     self,
     msg: str,
     tag: str,
     line: str,
-    element: str,
     original_exception: Exception,
+    **extra: Any,
   ):
     super().__init__(msg)
     self.tag = tag
     self.line = line
-    self.element = element
     self.original_exception = original_exception
+    for key, value in extra.items():
+      setattr(self, key, value)
+
+
+class SerializationError(Exception):
+  tag: str
+  original_exception: Exception
+
+  def __str__(self):
+        return f"{super().__str__()}\nTag: {self.tag}\nCaused by: {self.original_exception}"
+
+  def __init__(
+    self, msg: str, tag: str, original_exception: Exception, **extra: Any
+  ):
+    super().__init__(msg)
+    self.tag = tag
+    self.original_exception = original_exception
+    for key, value in extra.items():
+      setattr(self, key, value)
--- src/PythonTmx/utils.py
+++ src/PythonTmx/utils.py
@@ -1,5 +1,8 @@
 from os import PathLike
 from pathlib import Path
+from typing import Any, Never
+
+from PythonTmx.errors import ParsingError, SerializationError
 
 
 def ensure_file(path: PathLike[str] | Path | str) -> Path:
@@ -9,3 +12,88 @@ def ensure_file(path: PathLike[str] | Path | str) -> Path:
   if not path.is_file():
     raise IsADirectoryError(f"file {path} is not a file")
   return path
+
+
+def raise_serialization_errors(
+  tag: str,
+  error: Exception,
+  **extra: Any,
+) -> Never:
+  """
+  Centralized handler for mapping errors during element serialization to a `SerializationError`.
+
+  Args:
+    tag: The XML tag being parsed.
+    line: The line number in the XML file.
+    error: The exception to handle.
+    **extra: Optional extra context for error reporting (e.g., missing attribute, value, expected/actual types).
+
+  Raises:
+      SerializationError: Always, with a descriptive message and context and raised from the original exception.
+  """
+  match error:
+    case KeyError():
+      raise SerializationError(
+        f"Unexpected required attribute missing.\ntag: {tag}\nmissing attribute: {extra.get('missing', 'Unknown')}",
+        tag,
+        error,
+        **extra,
+      ) from error
+    case ValueError():
+      raise SerializationError(
+        f"Unexpected or missing value encountered.\ntag: {tag}\nIncorrect value: {extra.get('value', 'Unknown')}",
+        tag,
+        error,
+        **extra,
+      ) from error
+    case TypeError():
+      raise SerializationError(
+        f"Unexpected data type encountered.\ntag: {tag}\nexpected type: {extra.get('expected', 'Unknown')}\nactual type: {extra.get('actual', 'Unknown')}",
+        tag,
+        error,
+        **extra,
+      ) from error
+    case _:
+      raise SerializationError(
+        f"Unknown error:\ntag: {tag}",
+        tag,
+        error,
+        **extra,
+      ) from error
+
+
+def raise_parsing_errors(
+  tag: str,
+  line: str,
+  error: Exception,
+  **extra: Any,
+) -> Never:
+  """
+  Centralized handler for mapping errors during element parsing to a `ParsingError`.
+
+  Args:
+    tag: The XML tag being parsed.
+    line: The line number in the XML file.
+    error: The exception to handle.
+    **extra: Optional extra context for error reporting (e.g., missing attribute, value, expected/actual types).
+
+  Raises:
+      ParsingError: Always, with a descriptive message and context and raised from the original exception.
+  """
+  match error:
+    case SerializationError():
+      raise ParsingError(
+        f"Error at line {line} parsing <{tag}>:",
+        tag,
+        line,
+        error,
+        **extra,
+      ) from error
+    case _:
+      raise ParsingError(
+        f"Unknown error at line {line} parsing <{tag}>:",
+        tag,
+        line,
+        error,
+        **extra,
+      )
