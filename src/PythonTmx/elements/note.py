ca1aed5c75010f895e4e4dd9fd90be86d8095f11
Enzo Agosta <agosta.enzowork@gmail.com>
2025-07-25 11:09:39 +0200

    Add note

---
+++ src/PythonTmx/elements/note.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from PythonTmx.core import (
+  AnyElementFactory,
+  AnyXmlElement,
+  BaseTmxElement,
+  P,
+  R,
+)
+from PythonTmx.utils import (
+  ensure_element_structure,
+  ensure_required_attributes_are_present,
+  raise_serialization_errors,
+)
+
+
+@dataclass(slots=True)
+class Note(BaseTmxElement):
+  value: str
+  encoding: str | None = None
+  lang: str | None = None
+
+  @classmethod
+  def from_xml(cls: type[Note], element: AnyXmlElement) -> Note:
+    ensure_element_structure(element, expected_tag="prop")
+    if not element.text:
+      raise_serialization_errors(element.tag, ValueError(), missing="text")
+    ensure_required_attributes_are_present(element, ("type",))
+    try:
+      return cls(
+        value=element.text,
+        encoding=element.attrib.get("encoding", None),
+        lang=element.attrib.get(
+          "{http://www.w3.org/XML/1998/namespace}lang", None
+        ),
+      )
+    except Exception as e:
+      raise_serialization_errors(element.tag, e)
+
+  def to_xml(self, factory: AnyElementFactory[P, R]) -> R:
+    element = factory(
+      "note",
+      self._make_attrib_dict(),
+    )
+    element.text = self.value
+    return element
