b9aa0af3184140edc15ef3280b091ed2304c34ca
Enzo Agosta <agosta.enzowork@gmail.com>
2025-07-29 15:09:28 +0200

    add tu + tests

--- src/PythonTmx/elements/__init__.py
+++ src/PythonTmx/elements/__init__.py
@@ -3,11 +3,12 @@ from .inline import Bpt, Ept, Hi, It, Ph, Sub, Ut
 from .map import Map
 from .note import Note
 from .prop import Prop
+from .tu import Tu
 from .tuv import Tuv
 from .ude import Ude
 
 type InlineElement = Bpt | Ept | Hi | It | Ph | Sub | Ut
-type StructuralElement = Header | Ude | Map | Note | Prop | Tuv
+type StructuralElement = Header | Ude | Map | Note | Prop | Tu | Tuv
 
 __all__ = [
   "Prop",
@@ -15,6 +16,7 @@ __all__ = [
   "Map",
   "Ude",
   "Header",
+  "Tu",
   "Tuv",
   "Bpt",
   "Ept",
