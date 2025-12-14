d8dbba223eb1878e99b0726190a3a0ed5f38d5c2
Enzo Agosta <agosta.enzowork@gmail.com>
2025-12-14 18:32:40 +0100

    Add tests for encoding normalization and tag preparation functions

--- tests/xml/test_utils.py
+++ tests/xml/test_utils.py
@@ -26,3 +26,37 @@ def test_normalize_tag(val, ret):
 def test_normalize_tag_raises_on_unknown_type():
   with pytest.raises(TypeError, match="Unexpected tag type"):
     hm.normalize_tag(1)
+
+
+@pytest.mark.parametrize(
+  "val,ret",
+  [
+    (None, "utf-8"),
+    ("unicode", "utf-8"),
+    ("utf-8", "utf-8"),
+    ("UTF-8", "utf-8"),
+    ("utf8", "utf-8"),
+  ],
+  ids=["None", "unicode", "utf-8", "UTF-8", "utf8"],
+)
+def test_normalize_encoding(val, ret):
+  assert hm.normalize_encoding(val) == ret
+
+
+def test_normalize_encoding_raises_on_unknown_type():
+  with pytest.raises(ValueError, match="Unknown encoding: error"):
+    hm.normalize_encoding("error")
+
+
+@pytest.mark.parametrize(
+  "val,ret",
+  [
+    (None, None),
+    ("tag", {"tag"}),
+    (["tag"], {"tag"}),
+    (("tag", "tag2"), {"tag", "tag2"}),
+    (("{ns}tag", "{ns}tag2"), {"tag", "tag2"}),
+  ],
+)
+def test_prep_tag_set(val, ret):
+  assert hm.prep_tag_set(val) == ret
