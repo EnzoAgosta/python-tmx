17a97ceba0f54150d0f459405b7c47bdc9489749
Enzo Agosta <enzo.agosta@ninjarmm.com>
2025-12-19 18:15:06 +0100

    refactor tag and obecjt type checking to utils

--- src/hypomnema/xml/utils.py
+++ src/hypomnema/xml/utils.py
@@ -1,11 +1,10 @@
-from __future__ import annotations
+from hypomnema.base.errors import XmlSerializationError, InvalidTagError
+from hypomnema.xml.policy import SerializationPolicy, DeserializationPolicy
 from codecs import lookup
 from collections.abc import Collection
-from encodings import normalize_encoding as _normalize_encoding
-from typing import Any
-
-
-__all__ = ["normalize_tag", "normalize_encoding", "prep_tag_set"]
+from logging import Logger
+from typing import TypeIs, Any
+from encodings import normalize_encoding as python_normalize_encoding
 
 
 def normalize_tag(tag: Any) -> str:
@@ -36,9 +35,38 @@ def prep_tag_set(tags: str | Collection[str] | None) -> set[str] | None:
   if tags is None:
     return None
   if isinstance(tags, str):
+    if not len(tags):
+      return None
     tag_set = {normalize_tag(tags)}
   elif isinstance(tags, Collection):
     tag_set = set(normalize_tag(tag) for tag in tags)
   else:
     raise TypeError(f"Unexpected tag type: {type(tags)}")
   return tag_set or None
+
+
+def assert_object_type[ExpectedType](
+  obj: Any, expected_type: type[ExpectedType], *, logger: Logger, policy: SerializationPolicy
+) -> TypeIs[ExpectedType]:
+  if not isinstance(obj, expected_type):
+    logger.log(
+      policy.invalid_object_type.log_level,
+      "object of type %r is not an instance of %r",
+      obj.__class__.__name__,
+      expected_type.__name__,
+    )
+    if policy.invalid_object_type.behavior == "raise":
+      raise XmlSerializationError(
+        f"object of type {obj.__class__.__name__!r} is not an instance of {expected_type.__name__!r}"
+      )
+    return False
+  return True
+
+
+def check_tag(tag: str, expected_tag: str, logger: Logger, policy: DeserializationPolicy) -> None:
+  if not tag == expected_tag:
+    logger.log(
+      policy.invalid_tag.log_level, "Incorrect tag: expected %s, got %s", expected_tag, tag
+    )
+    if policy.invalid_tag.behavior == "raise":
+      raise InvalidTagError(f"Incorrect tag: expected {expected_tag}, got {tag}")
