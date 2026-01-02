07b7e059c3aa981818e44eb3b80c37f3efb23952
Enzo Agosta <115390908+EnzoAgosta@users.noreply.github.com>
2026-01-02 15:42:26 +0100

    Refactor type system to use generics for element content (#28)
    ### TL;DR
    
    Refactored TMX data model to use generics for better type safety and flexibility.
    
    ### What changed?
    
    - Converted TMX element classes to use generics for their content collections
    - Replaced specific list types with generic `Iterable` type variables
    - Added type aliases for common element combinations (`InlineElementAndStr`, `SubElementAndStr`)
    - Simplified imports in `__init__.py` to expose a cleaner public API
    - Removed redundant documentation comments from the type definitions
    - Added `XmlBackend` to the exported symbols
    
    ### Why make this change?
    
    This change improves the type system by making the TMX element classes more flexible while maintaining type safety. By using generics, the library can better accommodate different collection types and provide more precise type information to users. The refactoring also reduces code duplication and makes the codebase more maintainable.

--- src/hypomnema/__init__.py
+++ src/hypomnema/__init__.py
@@ -1,27 +1,4 @@
-from hypomnema.base import (
-  # Type aliases
-  BaseElement,
-  BaseInlineElement,
-  BaseStructuralElement,
-  # Enums
-  Pos,
-  Assoc,
-  Segtype,
-  # Structural elements
-  Tmx,
-  Header,
-  Prop,
-  Note,
-  Tu,
-  Tuv,
-  # Inline elements
-  Bpt,
-  Ept,
-  It,
-  Ph,
-  Sub,
-  Hi,
-  # Errors
+from hypomnema.base.errors import (
   XmlSerializationError,
   XmlDeserializationError,
   AttributeSerializationError,
@@ -30,9 +7,10 @@ from hypomnema.base import (
   InvalidContentError,
   MissingHandlerError,
 )
-
+import hypomnema.base.types as t
 from hypomnema.xml import (
   # Backends
+  XmlBackend,
   LxmlBackend,
   StandardBackend,
   # Deserialization
@@ -43,11 +21,29 @@ from hypomnema.xml import (
 
 from hypomnema.xml.policy import PolicyValue, DeserializationPolicy, SerializationPolicy
 
+BaseElement = t.BaseElement
+Note = t.Note
+Prop = t.Prop
+Assoc = t.Assoc
+Segtype = t.Segtype
+Pos = t.Pos
+
+Bpt = t.Bpt[list[t.SubElementAndStr]]
+Ept = t.Ept[list[t.SubElementAndStr]]
+It = t.It[list[t.SubElementAndStr]]
+Ph = t.Ph[list[t.SubElementAndStr]]
+Sub = t.Sub[list[t.InlineElementAndStr]]
+Hi = t.Hi[list[t.InlineElementAndStr]]
+
+Tuv = t.Tuv[list[t.Prop], list[t.Note], list[t.InlineElementAndStr]]
+Tu = t.Tu[list[t.Note], list[t.Prop], list[t.Tuv]]
+Header = t.Header[list[t.Prop], list[t.Note]]
+Tmx = t.Tmx[list[t.Tu]]
+
+
 __all__ = [
   # Type aliases
   "BaseElement",
-  "BaseInlineElement",
-  "BaseStructuralElement",
   # Enums
   "Pos",
   "Assoc",
@@ -75,6 +71,7 @@ __all__ = [
   "InvalidContentError",
   "MissingHandlerError",
   # Backends
+  "XmlBackend",
   "LxmlBackend",
   "StandardBackend",
   # Deserialization
